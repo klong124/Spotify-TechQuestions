b4600304ddb186ae5efd8b51b55727e885b91cee
Priya Bansal <banspri0@gmail.com>
2018-06-09 20:35:17 -0400

    Added test cases and fixed a tab issue

--- sortByStrings.py
+++ sortByStrings.py
@@ -4,4 +4,19 @@ def sortByStrings(s, t):
         for i in range(s.count(c)):
             result += c
             
-return ''.join(result)
+    return ''.join(result)
+    
+    
+print sortByStrings('weather', 'therapyw')
+print sortByStrings('good', 'odg')
+print sortByStrings('odg', 'god')
+print sortByStrings('tomorrow', 'wot')
+print sortByStrings('tomorrow', 'wat')
+print sortByStrings('tomorrow', 'what')
+print sortByStrings('', '')
+print sortByStrings('d', '')
+print sortByStrings('', 'e')
+print sortByStrings('dog', 'cat')
+print sortByStrings('dog', 'dog')
+print sortByStrings('dog', 'dogzilla')
+print sortByStrings('dogzilla', 'dog')
